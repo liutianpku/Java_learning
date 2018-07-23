06e1f660f7424600ba8b5b595f32774fe69caa50
Liu Tian <liutian@pku.edu.cn>
2018-07-23 16:56:58 +0800

    more about scanner difference between next() and nextLine()

--- src/ScannerTest.java
+++ src/ScannerTest.java
@@ -7,6 +7,16 @@ public class ScannerTest {
 		int a = scanner.nextInt();
 		System.out.printf("%d的平方是%d\n",a,a*a);
 		/************************************************/
+		 Scanner sc = new Scanner(System.in); 
+	     System.out.println("请输入你的姓名："); 
+	     String name = sc.nextLine(); 
+	     System.out.println("请输入你的年龄："); 
+	     int age = sc.nextInt(); 
+	     System.out.println("请输入你的工资："); 
+	     float salary = sc.nextFloat(); 
+	     System.out.println("你的信息如下："); 
+	     System.out.println("姓名："+name+"\n"+"年龄："+age+"\n"+"工资："+salary);
+		/************************************************/
 		char c = ' ';
 		System.out.print("Please input a char:");
 		try {
@@ -19,9 +29,9 @@ public class ScannerTest {
 		try{
 			BufferedReader in = new BufferedReader(
 				new InputStreamReader( System.in ) );
-			s1 = in.readLine();
-			s1 = in.readLine();
-		}catch(IOException e){}
+			s1 = in.readLine();//在java中，next()方法是不接收空格的，在接收到有效数据前，
+			s1 = in.readLine();//所有的空格或者tab键等输入被忽略，若有有效数据，则遇到这些键退出。
+		}catch(IOException e){}//nextLine()可以接收空格或者tab键，其输入应该以enter键结束。
 		System.out.println("You have entered: " + s1 );
 		/************************************************/
 		String s = "";
